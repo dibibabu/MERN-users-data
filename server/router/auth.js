5c0e3cffffde0bd363195e5210f321763d2e5879
dibi babu <dibikannan@gmail.com>
2022-12-12 16:52:04 +0530

    login started

--- server/router/auth.js
+++ server/router/auth.js
@@ -7,31 +7,85 @@ const User = require('../model/userSchema')
 router.get('/', (req, res) => {
     res.send("i am the router")
 })
+//PROMISE METHOD
+// router.post('/register', (req, res) => {
 
-router.post('/register', (req, res) => {
+//     const { name, email, phone, work, password, cpassword } = req.body
+
+//     if (!name || !email || !phone || !work || !password || !cpassword) {
+//         return res.status(402).json({ error: "pls fill aqquirate data" })
+//     }
+
+//     User.findOne({ email: email }).then((userExist) => {
+//         if (userExist) {
+//             return res.status(402).json({ error: "user allready exist" })
+//         }
+
+//         const user=new User({name, email, phone, work, password, cpassword})
+//         user.save().then(()=>{
+//             return res.status(201).json({ message: "user registered successfully" }).catch((err)=>{
+//                 res.status(500).json({error:"registration failed"})
+//             })
+//         }).catch((err)=>{
+//             console.log(err);
+//         })
+
+//     })
+
+// })
+router.post('/register', async (req, res) => {
 
     const { name, email, phone, work, password, cpassword } = req.body
 
     if (!name || !email || !phone || !work || !password || !cpassword) {
         return res.status(402).json({ error: "pls fill aqquirate data" })
     }
+    try {
 
-    User.findOne({ email: email }).then((userExist) => {
-        if (userExist) {
-            return res.status(402).json({ error: "user allready exist" })
-        }
+        const userExist=await User.findOne({ email: email });
+       
+                    if (userExist) {
+                         return res.status(422).json({ error: "user allready exist" })
+                     }
+        const user = new User({ name, email, phone, work, password, cpassword })
+         await user.save()
+        res.status(201).json({ message: "user registered successfully" })
+    } catch (err) {
 
-        const user=new User({name, email, phone, work, password, cpassword})
-        user.save().then(()=>{
-            return res.status(201).json({ message: "user registered successfully" }).catch((err)=>{
-                res.status(500).json({error:"registration failed"})
-            })
-        }).catch((err)=>{
-            console.log(err);
-        })
+        console.log(err);
 
-    })
+
+
+    }
+    
+})
+
+router.post('/login',async(req,res)=>{
+
+   try {
+    const {email,password}=req.body
+
+    if (!email||!password) {
+        res.status(400).json({"message":"pls fill"})
+        
+    }
+    const userLogin=await User.findOne({email:email})
+    console.log(userLogin);
+    if (!userLogin) {
+       
+        res.status(400).json({error:"No user found"})
+        
+    } else {
+         res.json({"message":"Login success"})
+       
+    }
+    
+   } catch (error) {
+    console.log(error);
+   }
 
 })
+
+
 
 module.exports = router;
