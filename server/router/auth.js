fcca8427f2582d705a371d7c73e40cb148704a0d
dibi babu <dibikannan@gmail.com>
2023-01-16 11:52:44 +0530

    new added

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
-const bcrypt=require('bcrypt')
+const bcrypt = require('bcrypt')
+const jwt=require('jsonwebtoken')
 
 require('../Db/conn')
 const User = require('../model/userSchema')
@@ -43,13 +44,13 @@ router.post('/register', async (req, res) => {
     }
     try {
 
-        const userExist=await User.findOne({ email: email });
-       
-                    if (userExist) {
-                         return res.status(422).json({ error: "user allready exist" })
-                     }
+        const userExist = await User.findOne({ email: email });
+
+        if (userExist) {
+            return res.status(422).json({ error: "user allready exist" })
+        }
         const user = new User({ name, email, phone, work, password, cpassword })
-         await user.save()
+        await user.save()
         res.status(201).json({ message: "user registered successfully" })
     } catch (err) {
 
@@ -58,38 +59,45 @@ router.post('/register', async (req, res) => {
 
 
     }
-    
+
 })
 
-router.post('/login',async(req,res)=>{
+router.post('/login', async (req, res) => {
 
-   try {
-    const {email,password}=req.body
+    try {
+        const { email, password } = req.body
 
-    if (!email||!password) {
-        res.status(400).json({"message":"pls fill"})
-        
+        if (!email || !password) {
+            res.status(400).json({ "message": "pls fill" })
+
+        }
+        const userLogin = await User.findOne({ email: email })
+        console.log(userLogin);
+        if (userLogin) {
+            const isMatch = await bcrypt.compare(password, userLogin.password)
+            const token=await userLogin.generateAuthToken()
+            console.log(token);
+            res.cookie("jwttoken",token,{
+               expires:new Date(Date.now()+2589200000),
+               httpOnly:true 
+            })
+
+            if (!isMatch) {
+
+                res.status(400).json({ error: "No user found" })
+
+            } else {
+                res.json({ "message": "Login success" })
+
+            }
+        } else {
+            res.json({ "message": "user does not exist" })
+        }
+
+
+    } catch (error) {
+        console.log(error);
     }
-    const userLogin=await User.findOne({email:email})
-    console.log(userLogin);
-    if (userLogin) {
-        const isMatch=await bcrypt.compare(password,userLogin.password)
-    if (!isMatch) {
-       
-        res.status(400).json({error:"No user found"})
-        
-    } else {
-         res.json({"message":"Login success"})
-       
-    }
-    } else {
-        res.json({"message":"user does not exist"})
-    }
-    
-    
-   } catch (error) {
-    console.log(error);
-   }
 
 })
 
